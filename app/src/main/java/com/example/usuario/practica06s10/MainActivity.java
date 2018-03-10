369c65e663f54d8fd9ad6e5fbea117c44559ce26
kiritoedson <edson_beatle@hotmail.com>
2018-03-10 10:43:57 -0600

    algo

--- app/src/main/java/com/example/usuario/practica06s10/MainActivity.java
+++ app/src/main/java/com/example/usuario/practica06s10/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.usuario.practica06s10;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -54,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             button.setBackgroundResource(R.drawable.buttonstyle);
             button.setId(i);
             button.setOnClickListener(this);
+            registerForContextMenu(button);
             ll1.addView(button);
         }
     }
@@ -66,12 +69,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i = new Intent(Intent.ACTION_DIAL);
             startActivity(i);
         }
-        if (id==6){
-            for (int i=0;i<stringbtn.length;i++){
-                Button boton=findViewById(i);
+        if (id == 6) {
+            for (int i = 0; i < stringbtn.length; i++) {
+                Button boton = findViewById(i);
                 boton.setBackgroundResource(R.drawable.buttonstyle2);
+                boton.setTextColor(Color.WHITE);
             }
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        Button boton = findViewById(v.getId());
+        menu.setHeaderTitle(boton.getText().toString());
+        menu.setHeaderIcon(getResources().getDrawable(android.R.drawable.ic_dialog_email));
+        menu.add(0, v.getId(), 0, "Upload");
+        menu.add(0, v.getId(), 0, "Search");
+        menu.add(0, v.getId(), 0, "Share");
+        menu.add(0, v.getId(), 0, "Bookmark");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(getApplicationContext(), "item seleccionado: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        if (item.getTitle() == "Share") {
+            Intent i=new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("http://google.com"));
+            startActivity(i);
+        }
+        return super.onContextItemSelected(item);
     }
 
     @Override
